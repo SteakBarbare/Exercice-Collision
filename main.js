e49d6a34a4b80057add5a368bc45ecb1befaf2c7
Corto Dufour <corto.dufour-fakhouri@hetic.net>
2023-11-07 10:14:09 +0100

    Correction

--- main.js
+++ main.js
@@ -4,15 +4,19 @@ const luigi = document.querySelector("#luigi");
 
 // getBoundingClientRect récupère toutes les infos utiles sur l'image et sa boite de collision
 console.log(mario.getBoundingClientRect());
+console.log(mario);
 
 // On détecte la pression d'une touche
 document.addEventListener("keydown", moveMario);
 
 function moveMario(e) {
+  // console.log(e);
   switch (e.keyCode) {
     // On déplace Mario en fonction de la touche pressée
     case 39:
-      mario.style.left = mario.getBoundingClientRect().x + 6 + "px";
+      if (!isCollision(mario, luigi, 6, 0)) {
+        mario.style.left = mario.getBoundingClientRect().x + 6 + "px";
+      }
       break;
     case 37:
       mario.style.left = mario.getBoundingClientRect().x - 6 + "px";
@@ -23,5 +27,24 @@ function moveMario(e) {
     case 38:
       mario.style.top = mario.getBoundingClientRect().y - 6 + "px";
       break;
+  }
+}
+
+function isCollision(element1, element2, xSpeed, ySpeed) {
+  let marioBounding = element1.getBoundingClientRect();
+  let luigiBounding = element2.getBoundingClientRect();
+
+  // On check si Mario + sa taille dépasse l'origine en x de Luigi
+  // On check si l'origine de Mario dépasse Luigi + sa taille
+  // Same pour les y
+  if (
+    marioBounding.x + marioBounding.width + xSpeed >= luigiBounding.x &&
+    marioBounding.x + xSpeed <= luigiBounding.x + luigiBounding.width &&
+    marioBounding.y + marioBounding.height + ySpeed >= luigiBounding.y &&
+    marioBounding.y + ySpeed <= luigiBounding.y + luigiBounding.height
+  ) {
+    return true;
+  } else {
+    return false;
   }
 }
